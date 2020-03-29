f7e825589b9a6882611be1b3c05cde8d9ba309cd
Phillip Truppelli <56132382+ptruppel@users.noreply.github.com>
2020-03-29 18:20:30 -0400

    Update BearingStressUpdated.m

--- Code/BearingStressUpdated.m
+++ Code/BearingStressUpdated.m
@@ -4,17 +4,20 @@
 
 clear; 
 clc;
-f = input('where f is the major diameter of the bolt');
-i = input('where i is the inside diameter (minor diameter) of the nut');
+f = input('Enter f, where f is the major diameter of the bolt: ');
+i = input('Enter i, where i is the inside diameter (minor diameter) of the nut: ');
 A=(pi/4).*((f.^2)-(i.^2));
 A = abs(A);
 disp(A);
 
-P = input('is pressure on bolt');
-p = input('is pitch of threads');
-t = input('is nut thickness');
+P = input('Enter pressure on bolt:');
+p = input('Enter pitch of threads: ');
+t = input('Enter nut thickness: ');
 B=(P./A).*(p./t);
 B
 
-T = input('is maximum stress before shearing')
-S=(T./B)
+T = input('Enter maximum stress before shearing: ')
+S=(T./B) % Safety factor calculated
+
+% More information on this analysis can be seen at the link below on pg. 3.
+% http://portal.ku.edu.tr/~cbasdogan/Courses/MDesign/course_notes/ScrewStresses.pdf
